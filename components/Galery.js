e286294db4ac9a6ca14e24e829c2187698694fd4
Gustavo Letério <gustavoleterio@gmail.com>
2021-10-12 19:56:48 -0300

    turning DB in array

--- components/Galery.js
+++ components/Galery.js
@@ -1,13 +1,11 @@
 import styles from '../styles/galery.module.css'
-import ProjectLines from '../components/ProjectLines'
 
 const Galery = () => {
     return (
         <div className={styles.galeryPage}>
             <h1 className={styles.title}>Projetos</h1>
             <div className={styles.galery}>
-                <ProjectLines/>
-                <ProjectLines/>
+
             </div>
         </div>
     );
