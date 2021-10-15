c60d505f78adb5c5b065560aab713fc0fd4533f5
Gustavo Letério <gustavoleterio@gmail.com>
2021-10-15 19:59:50 -0300

    getStaticProps Solving

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import Link from 'next/link'
 
 export const getStaticProps = async () => {
 
-  const res = await fetch("https://my-json-server.typicode.com/Leterinho/PortfolioInteriorDesign/db");
+  const res = await fetch("https://my-json-server.typicode.com/Leterinho/PortfolioInteriorDesign/cards");
   const datas = await res.json();
 
   return {
@@ -36,14 +36,15 @@ export default function Home({ datas }) {
               <div className={styles.lineWrapper} key={data.categoryName}>
                 <a className={styles.leftArrow}>&#10094;</a>
                 <div className={styles.line} >
-                  <div className={styles.imageBox} >
-                    <Image src={data.image} blurDataURL={data.image} width={400} height={200} layout="responsive" lazy="true" placeholder="blur" />
-                    <div className={styles.linkContent}>
-                      <span className={styles.name}>{data.name}</span>
-                      <Link href=""><a className={styles.link}>Veja Mais!</a></Link>
+                  {datas.map((data) => (
+                    <div className={styles.imageBox} >
+                      <Image src={data.image} blurDataURL={data.image} width={400} height={200} layout="responsive" lazy="true" placeholder="blur" />
+                      <div className={styles.linkContent}>
+                        <span className={styles.name}>{data.name}</span>
+                        <Link href=""><a className={styles.link}>Veja Mais!</a></Link>
+                      </div>
                     </div>
-                  </div>
-
+                  ))}
                 </div>
                 <a className={styles.rightArrow}>&#10095;</a>
               </div>
