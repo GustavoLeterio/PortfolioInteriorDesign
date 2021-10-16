f7d94ce40f4627f1eb0e3a1b0b167ff037fa138f
Gustavo Letério <gustavoleterio@gmail.com>
2021-10-16 16:55:35 -0300

    getStaticProps Solving

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import Link from 'next/link'
 
 export const getStaticProps = async () => {
 
-  const res = await fetch("https://my-json-server.typicode.com/Leterinho/PortfolioInteriorDesign/db");
+  const res = await fetch("https://my-json-server.typicode.com/Leterinho/PortfolioInteriorDesign/office");
   const datas = await res.json();
 
   return {
@@ -18,32 +18,6 @@ export const getStaticProps = async () => {
 
 export default function Home({ datas }) {
 
-  function Test(data) {
-    return (
-      <div className={styles.imageBox} key={data.id}>
-        <Image src={data.image} blurDataURL={data.image} width={400} height={200} layout="responsive" lazy="true" placeholder="blur" />
-        <div className={styles.linkContent}>
-          <span className={styles.name}>{data.name}</span>
-          <Link href=""><a className={styles.link}>Veja Mais!</a></Link>
-        </div>
-      </div>
-    )
-  }
-
-  function line(data, category) {
-    return (
-      <div className={styles.categoryWrapper} >
-        <h4 className={styles.subTitle}>{category}</h4>
-        <div className={styles.lineWrapper}>
-          <a className={styles.leftArrow}>&#10094;</a>
-          <div className={styles.line} >
-            {data.map((data) => (<>{Test(data)}</>))}
-          </div>
-          <a className={styles.rightArrow}>&#10095;</a>
-        </div>
-      </div>
-    )
-  }
   return (
     <>
       <Head>
@@ -57,8 +31,24 @@ export default function Home({ datas }) {
       <div className={styles.galeryPage}>
         <h1 className={styles.title}>Projetos</h1>
         <div className={styles.galery}>
-          {line(datas.office, "Escritório")}
-          {line(datas.livingRoom, "Escritório")}
+          <div className={styles.categoryWrapper} >
+            <h4 className={styles.subTitle}>a</h4>
+            <div className={styles.lineWrapper}>
+              <a className={styles.leftArrow}>&#10094;</a>
+              <div className={styles.line} >
+                {datas.map((data) => (
+                  <div className={styles.imageBox} key={data.id}>
+                    <Image src={data.image} blurDataURL={data.image} width={400} height={200} layout="responsive" lazy="true" placeholder="blur" />
+                    <div className={styles.linkContent}>
+                      <span className={styles.name}>{data.name}</span>
+                      <Link href=""><a className={styles.link}>Veja Mais!</a></Link>
+                    </div>
+                  </div>
+                ))}
+              </div>
+              <a className={styles.rightArrow}>&#10095;</a>
+            </div>
+          </div>
         </div>
       </div>
     </>
