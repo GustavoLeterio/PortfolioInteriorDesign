f4a7621bffd6de89a3d7749cb725a51a76926f06
Gustavo Letério <gustavoleterio@gmail.com>
2021-10-16 17:11:43 -0300

    getStaticProps Solving

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,48 @@ import Head from 'next/head'
 import Navbar from '../components/Navbar'
 import LandingPage from '../components/LandingPage'
 import About from '../components/About'
+import styles from '../styles/galery.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
 
+export const getStaticProps = async () => {
 
-export default function Home() {
+  const res = await fetch("https://my-json-server.typicode.com/Leterinho/PortfolioInteriorDesign/db");
+  const datas = await res.json();
 
+  return {
+    props: { datas }
+  }
+}
+
+export default function Home({ datas }) {
+
+  function Test(mod) {
+    return (
+      <div className={styles.imageBox} key={mod.id}>
+        <Image src={mod.image} blurDataURL={mod.image} width={400} height={200} layout="responsive" lazy="true" placeholder="blur" />
+        <div className={styles.linkContent}>
+          <span className={styles.name}>{mod.name}</span>
+          <Link href=""><a className={styles.link}>Veja Mais!</a></Link>
+        </div>
+      </div>
+    )
+  }
+
+  function line(data, category) {
+    return (
+      <div className={styles.categoryWrapper} >
+        <h4 className={styles.subTitle}>{category}</h4>
+        <div className={styles.lineWrapper}>
+          <a className={styles.leftArrow}>&#10094;</a>
+          <div className={styles.line} >
+            {data.map((mod) => (<>{Test(mod)}</>))}
+          </div>
+          <a className={styles.rightArrow}>&#10095;</a>
+        </div>
+      </div>
+    )
+  }
   return (
     <>
       <Head>
@@ -16,6 +54,13 @@ export default function Home() {
       <Navbar />
       <LandingPage />
       <About />
+      <div className={styles.galeryPage}>
+        <h1 className={styles.title}>Projetos</h1>
+        <div className={styles.galery}>
+          {line(datas.office, "Escritório")}
+          {line(datas.livingRoom, "Sala de Estar")}
+        </div>
+      </div>
     </>
   )
 }
