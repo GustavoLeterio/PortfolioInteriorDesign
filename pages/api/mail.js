fb0d5e0cfba0ccf051df2ebb8ae65b5d5eec8ca6
Gustavo Letério <gustavoleterio@gmail.com>
2021-10-26 21:13:05 -0300

    Send Grid API implementation

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -3,22 +3,22 @@ const mail = require('@sendgrid/mail');
 mail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export default (req, res) => {
-    const body = JSON.parse(req.body);
+  const body = JSON.parse(req.body);
 
-    const message = `
+  const message = `
     Name: ${body.name} ${body.lastName}\r\n
     Email: ${body.email}\r\n
     Email: ${body.phoneNumber}\r\n
     Message: ${body.message}
   `;
 
-    await mail.send({
-        to: 'juliacostadesigndeinteriores@gmail.com',
-        from: 'juliacostadesigndeinteriores@gmail.com',
-        subject: 'Entraram em contato meu amor! ❤️ ❤️ ❤️',
-        text: message,
-        html: message.replace(/\r\n/g, '<br>')
-    });
+  await mail.send({
+    to: 'juliacostadesigndeinteriores@gmail.com',
+    from: 'juliacostadesigndeinteriores@gmail.com',
+    subject: 'Entraram em contato meu amor! ❤️ ❤️ ❤️',
+    text: message,
+    html: message.replace(/\r\n/g, '<br>'),
+  });
 
-    res.status(200).json({ status: 'ok' });
+  res.status(200).json({ status: 'ok' });
 }
