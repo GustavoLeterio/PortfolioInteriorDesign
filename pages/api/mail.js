ad635e1a40d41c7c582109b96a95669d63d1c775
Gustavo Letério <gustavoleterio@gmail.com>
2021-10-30 13:40:41 -0300

    sendGrid API correction

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -2,7 +2,7 @@ const mail = require('@sendgrid/mail');
 
 mail.setApiKey(process.env.SENDGRID_API_KEY);
 
-export default (req, res) => {
+export default async(req, res) => {
   const body = JSON.parse(req.body);
 
   const message = `
